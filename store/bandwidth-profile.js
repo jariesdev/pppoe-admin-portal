6811dd4d3931f852012a5782c7d16b91d1b39e3e
Jay Aries Flores <aries@esixtycorporation.com>
2021-05-23 01:52:33 +0800

    Updated bandwidth profiles

--- store/bandwidth-profile.js
+++ store/bandwidth-profile.js
@@ -12,14 +12,14 @@ export const mutations = {
 
 export const actions = {
   async load ({ commit }) {
-    const { data } = await this.$axios.$get('/api/bandwidth-profiles', { params: { perPage: -1 } })
+    const { data } = await this.$axios.$get('/api/pppoe-profiles', { params: { perPage: -1 } })
     commit('setBandwidthProfiles', data)
     return data
   },
-  async delete ({}, id) {
-    return await this.$axios.$delete('/api/bandwidth-profiles/' + id)
+  async delete (_, id) {
+    return await this.$axios.$delete('/api/pppoe-profiles/' + id)
   },
-  async get ({}, id) {
-    return await this.$axios.$get('/api/bandwidth-profiles/' + id).then(({ data }) => data)
+  async get (_, id) {
+    return await this.$axios.$get('/api/pppoe-profiles/' + id).then(({ data }) => data)
   }
 }
