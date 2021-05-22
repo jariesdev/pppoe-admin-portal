018843d124bb734b5d3c14ff20a6d0de1de05979
Jay Aries Flores <aries@esixtycorporation.com>
2021-05-22 13:48:07 +0800

    Autofocus username

--- pages/login.vue
+++ pages/login.vue
@@ -23,6 +23,7 @@
                 v-model="login.username"
                 addon-left-icon="tim-icons icon-email-85"
                 type="text"
+                autofocus
               />
             </div>
             <div class="form-group">
