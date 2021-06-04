bac6dfddfc849743e566d5674808e02296c6b978
Jay Aries Flores <aries@esixtycorporation.com>
2021-06-04 15:26:42 +0800

    Add account settings

--- pages/customers/_customerId/index.vue
+++ pages/customers/_customerId/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="customer">
-    <CustomerDetails :customer-id="customerId" />
+    <el-tabs v-model="activeTab">
+      <el-tab-pane name="details" label="Details">
+        <CustomerDetails :customer-id="customerId" />
+      </el-tab-pane>
+      <el-tab-pane name="account-settings" label="Account Settings">
+        <CustomerAccountSettings :customer-id="customerId" />
+      </el-tab-pane>
+    </el-tabs>
     <div>
       <base-button @click="$router.back()">
         Back
@@ -10,9 +17,14 @@
 </template>
 <script>
 import CustomerDetails from '~/components/Content/Customer/CustomerDetails'
+import CustomerAccountSettings from '~/components/Content/Customer/CustomerAccountSettings'
 
 export default {
-  components: { CustomerDetails },
+  components: { CustomerAccountSettings, CustomerDetails },
+  data: () => ({
+    customerId: null,
+    activeTab: 'details'
+  }),
   fetch () {
     this.customerId = parseInt(this.$route.params.customerId)
   }
