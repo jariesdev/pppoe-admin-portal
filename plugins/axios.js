f696fa38090f53934077b8cfafa052d1ac1dbb17
Jay Aries Flores <aries@esixtycorporation.com>
2021-07-01 10:59:35 +0800

    Fixed logout on page reload; Fixed login error while router base is set;

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,11 @@ export default function ({ $axios, redirect }) {
 
   $axios.onRequest((config) => {
     // fixed proxy request from server which, axios not using proxy pathRewrite in server so we manually set it here
-    config.url = process.server ? config.url.replace(/^\/api\//, '/api/admin/') : config.url
+    if (process.server) {
+      config.url = config.url.replace(/^\/api\//, '/api/admin/')
+    } else {
+      config.url = process.env.routerBase ? process.env.routerBase + config.url : config.url
+    }
   })
 
   $axios.onError((error) => {
